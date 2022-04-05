7157e7556563ac101011c46fb0aaca291d8f4b46
James Lingham <James@rhythmmax.co.uk>
2022-04-05 13:40:16 +0100

    Update event_example.js

--- event_example/event_example.js
+++ event_example/event_example.js
@@ -3,7 +3,7 @@ import { event } from "tealium";
 console.log("this is our example event function");
 console.log({{att_test}})
 const attribute_list = {{att_list}};
-
+const data = {{key_pair}}
 att_list.forEach(a=>{
   console.log(a);
 })
