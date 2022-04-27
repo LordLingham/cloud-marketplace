dd84557d231031a4e32853617a1ee0514f1f3a66
James Lingham <James@rhythmmax.co.uk>
2022-04-27 10:27:06 +0200

    Create everything.js

---
+++ everything/everything.js
@@ -0,0 +1,28 @@
+
+//bool
+const bool_test = {{bool_test}};
+
+// userlist
+const userlist = {{userlist_test}};
+
+// keypair
+const keypair = {{keypair_test}}
+
+//text 
+const text = {{text_test}}
+
+//number
+const number = {{number_test}}
+
+// userlist
+const userlist = {{userlist_test}};
+
+// list_test
+const list = {{list_test}}
+
+// attribute
+const = attribute = {{att_test}}
+
+// attribute list
+const attribute_list = {{att_list}}
+
